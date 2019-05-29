836cee78ffd4664d46d9d04d74d6eef4c8a0938e
fwolf <fwolf@posteo.de>
2019-05-29 11:12:44 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,87 @@
+# PURPOSE
+
+This database enables Sparkify to query what songs users are listening to.
+Sparkify collects data as song_data and log_data which comprise the data foundation fo its analytics endeavor.
+The song data consists of songs and artists data. song_data organized by the first three letters of the Track IDs. The log data contains information about users, and the time a users listens to particular songs of particular artists. log_data is organised by year, month and day.
+
+# <a name="schema"></a>SCHEMA DESIGN
+
+To enable Sparkify to search songs users listen to a star database schema was used.
+songplays functions as the facts table. 
+
+
+<p>
+    <img src="images/songplays.JPG" width="220" height="240" />
+</p>
+
+Songlays table
+![alt text](images/songplays.JPG "Songlays table")
+
+songs, artists, users and time are separate dimension tables whose data is connected to the facts data by means of foreign key relations using song_id, artist_id, users_id and start_time.
+
+Songs table
+![alt text](images/songs.png "Songs Table")
+
+Artists table
+![alt text](images/artists.png "Artists Table")
+
+Users table
+![alt text](images/users.png  "Users Table" )
+
+Time table
+![alt text](images/time.png "Time Table")
+
+
+# ETL PIPELINE
+
+The data to fill the outline schema comes from two input file types:
+* song_data
+* log_data
+
+From song_data files that are ordered in folders named after the first three track letters, contain the data to fill the songs and artists table.
+Each file contains data about one song and its artist in JSON format.
+
+Song_data file - example.
+<br>
+![alt text](images/song_data_file.png "Song_data file json")
+
+From log_data files that are ordered in folders by year and month contain the data to fill the users, time and songplays table.
+Each files contains all songplay logs having been logged on one day. So each contains enties for each songplay in JSON.
+Each file contains data for the songplays, users and time table.
+
+Log_data file - An example log in one of the files in JSON.
+![alt text](images/log_data_file_json_example.png "Log_data file json")
+
+## ETL Process:
+
+### Overview and files.
+The database as well as ETL process belonging to is implemented using PostgreSQL und Pythons (psycopg2).
+The ETL process consists of:
+* database and table creation ( create_tables.py ) 
+* data ingestion using etl.py
+
+Both parts use PostgreSQL queries that are pre-defined in sql-queries.py.
+
+### ETL Description
+
+creates_table.py.
+
+Executing creates_table.py drops tables if they exists and (re-)creates the tables specified in [Link-Text.](#schema)
+
+
+
+# EXAMPLE QUERIES
+
+Returns songs and how often they where played 
+
+%%sql SELECT songs.title, artists.name, count(*)
+FROM songplays, songs, artists
+WHERE 
+songplays.song_id = songs.song_id AND 
+songplays.artist_id = artists.artist_id
+GROUP BY(songs.song_id, artists.artist_id)
+
+The readme looks fine.. I would encourage you to use images, like screenshots of your final tables, and adding more details about your project, such as the dataset and how you cleaned the data. I’d also encourage you to remove any comments that are part of the Instructions. Your readme file should reflect atleast the following specifications:
+
+Purpose of this database in the context of the startup, Sparkify, and their analytical goals.
+State and justify your database schema design and ETL pipeline.
---
+++ sql_queries.py
@@ -0,0 +1,78 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
+                                songplay_id SERIAL PRIMARY KEY,
+                                start_time VARCHAR NOT NULL,
+                                user_id INTEGER,
+                                song_id VARCHAR,
+                                artist_id VARCHAR,
+                                session_id INTEGER,
+                                location VARCHAR,
+                                user_agent VARCHAR);""")
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
+                            user_id INTEGER PRIMARY KEY,
+                            first_name VARCHAR NOT NULL, 
+                            last_name VARCHAR NOT NULL,
+                            gender VARCHAR NOT NULL,
+                            level VARCHAR NOT NULL);""")
+
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
+                            song_id VARCHAR PRIMARY KEY, 
+                            title VARCHAR NOT NULL,
+                            artist_id VARCHAR NOT NULL, 
+                            year INTEGER,
+                            duration NUMERIC NOT NULL);""")
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
+                            artist_id VARCHAR PRIMARY KEY,
+                            name VARCHAR NOT NULL,
+                            location VARCHAR,
+                            latitude NUMERIC,
+                            longitude NUMERIC);""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
+                            start_time VARCHAR NOT NULL,
+                            hour INTEGER NOT NULL CHECK (hour > -1 AND hour < 24),
+                            day INTEGER NOT NULL,
+                            week INTEGER NOT NULL,
+                            month INTEGER NOT NULL,
+                            year INTEGER NOT NULL,
+                            weekday INTEGER NOT NULL);""")
+
+
+# INSERT RECORDS
+
+
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, song_id, artist_id, 
+                            session_id, location, user_agent)
+                                VALUES (%s, %s, %s, %s, %s, %s, %s);""")
+
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+                            VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE set level=excluded.level, last_name = excluded.last_name, gender = excluded.gender;""")
+
+song_table_insert = ("""INSERT INTO songs as so (song_id, title, artist_id, year, duration) 
+                            VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO UPDATE set year=excluded.year WHERE so.year = 0;""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
+                            VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO UPDATE set location=excluded.location, latitude=excluded.latitude, longitude=excluded.longitude;""")
+
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s);""")
+
+# FIND SONGS
+
+song_select = ("""SELECT s.song_id, a.artist_id FROM songs as s JOIN artists as a ON s.artist_id = a.artist_id WHERE s.title = %s and a.name = %s and s.duration = %s ;""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
